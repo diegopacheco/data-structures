65a03eea26d9f664a8235828c42cff4d29793ddc
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-09 01:10:02 -0800

    c++/single-linked-list

--- cpp/single-linked-list/src/main.cpp
+++ cpp/single-linked-list/src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+struct Node{
+    int data;
+    Node* next;
+};
+
+void insertAtBegin(Node*& head, int data){
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
+}
+
 int main(){
     cout << "C++ works" << endl;
     return 0;
