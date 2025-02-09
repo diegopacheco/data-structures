cca541f68cf006e280222e1722a3a354bd2932c3
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-09 01:43:20 -0800

    kotlin/single-linked-list

--- kotlin/single-linked-list/src/main/kotlin/MainKT.kt
+++ kotlin/single-linked-list/src/main/kotlin/MainKT.kt
@@ -1,3 +1,86 @@
-fun main(){
-  println("hello World Kotlin")
+class Node<T>(var data: T, var next: Node<T>? = null)
+
+class LinkedList<T> {
+  var head: Node<T>? = null
+
+  fun insertAtBegin(data: T) {
+    val newNode = Node(data)
+    newNode.next = head
+    head = newNode
+  }
+
+  fun insertAtEnd(data: T) {
+    val newNode = Node(data)
+    if (head == null) {
+      head = newNode
+    } else {
+      var current = head
+      while (current?.next != null) {
+        current = current.next
+      }
+      current?.next = newNode
+    }
+  }
+
+  fun deleteNode(data: T) {
+    if (head == null) return
+
+    if (head?.data == data) {
+      head = head?.next
+      return
+    }
+
+    var current = head
+    while (current?.next != null && current.next?.data != data) {
+      current = current.next
+    }
+
+    if (current?.next != null) {
+      current.next = current.next?.next
+    }
+  }
+
+  fun searchNode(data: T): Boolean {
+    var current = head
+    while (current != null) {
+      if (current.data == data) {
+        return true
+      }
+      current = current.next
+    }
+    return false
+  }
+
+  fun printList() {
+    var current = head
+    while (current != null) {
+      print("${current.data} ")
+      current = current.next
+    }
+    println()
+  }
+}
+
+fun main() {
+  println("Linked List Example in Kotlin")
+
+  val list = LinkedList<Int>()
+  list.insertAtBegin(30)
+  list.insertAtBegin(20)
+  list.insertAtBegin(10)
+  list.insertAtEnd(40)
+  list.insertAtEnd(50)
+
+  println("List: ")
+  list.printList()
+
+  list.deleteNode(40)
+  println("List after deleting 40: ")
+  list.printList()
+
+  if (list.searchNode(4)) {
+    println("4 is in the list")
+  } else {
+    println("4 is not in the list")
+  }
 }
