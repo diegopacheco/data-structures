e11e4cc072f17e9e3928e602c8a56cec452ccc49
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-09 22:26:37 -0800

    zig/stack

--- zig/stack/src/main.zig
+++ zig/stack/src/main.zig
@@ -1,26 +1,24 @@
 const std = @import("std");
-const Stack = @import("stack.zig");
+const Stack = @import("stack.zig").Stack;
 
 pub fn main() !void {
-    var stack = Stack.init(i32, 5);
-
+    var stack = Stack(i32, 5).init();
     try stack.push(100);
     try stack.push(200);
     try stack.push(300);
-    stack.print_stack();
+    stack.printStack();
 
     std.debug.print("Top element: {?}\n", .{stack.peek()});
-
     const popped1 = try stack.pop();
-    std.debug.print("Popped: {?}\n", .{popped1});
+    std.debug.print("Pop: {}\n", .{popped1});
 
     const popped2 = try stack.pop();
-    std.debug.print("Popped: {?}\n", .{popped2});
+    std.debug.print("Pop: {}\n", .{popped2});
 
     if (stack.isEmpty()) {
         std.debug.print("Stack is empty.\n", .{});
     } else {
         std.debug.print("Stack is not empty.\n", .{});
     }
-    stack.print_stack();
+    stack.printStack();
 }
